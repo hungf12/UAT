d4a818c3ac7fc2c1d4071ce0731b9741ae3a1d60
hungf12 <93297529+hungf12@users.noreply.github.com>
2024-09-22 17:02:09 +0700

    update tools clean transcrip
    ok

--- Tools clean transcript.py
+++ Tools clean transcript.py
@@ -10,62 +10,64 @@ import re
 def submit_info():
     base_folder = entry_base_folder.get()
     regex = r'^[a-zA-Z]:[\\\/]([^\\\/:*?"<>|]+[\\\/])*[^\\\/:*?"<>|]*$'
-    if re.match(base_folder, regex):
-        merged_data = []
-
-        # Walk through all folders and subfolders
-        for root, dirs, files in os.walk(base_folder):
-            for file in files:
-                if file.endswith('.json'):
-                    file_path = os.path.join(root, file)
-                    # Open and read each JSON file
-                    with open(file_path, 'r', encoding='utf-8') as f:
-                        data = json.load(f)
-                        # If the data is a list, extend the merged list, otherwise append
-                        if isinstance(data, list):
-                            merged_data.extend(data)
-                        else:
-                            merged_data.append(data)
-
-        def clean_content(content):
-            if len(content) == 0:
-                return None
-            content_df = pd.DataFrame(content)[["channel", "text"]]
-            return content_df.to_dict("records")
-
-        def clean_meta(metadata):
-            metadata = eval(
-                metadata.replace("true", "True")
-                .replace("false", "False")
-                .replace("null", "None")
-            )["public"]
-            return metadata
-
-        # Write the merged data to a new JSON file
-        path = Path(base_folder)
-        file_name = path.name
-        output_file = f'C:/Users/Admin/Downloads/test/{file_name}.json'
-        with open(output_file, 'w', encoding='utf-8') as f:
-            json.dump(merged_data, f, indent=4, ensure_ascii=False)
-        # print(f"Data from JSON files in multiple folders has been merged into {output_file}")
-
-        if __name__ == "__main__":
-            file_name = output_file
-            data_call = pd.read_json(file_name)[["name", "agentChannel", "content", "metadata"]]
-
-            data_call["content"] = data_call["content"].apply(clean_content)
-            data_call["metadata"] = data_call["metadata"].apply(clean_meta)
-            data_call.to_json(
-                file_name.split(".")[0] + "_clean.json",
-                orient="records",
-                force_ascii=False,
-                indent=2,
-            )
-        messagebox.showinfo("Successfully", f"Name Transcript: {file_name}")
-        folder_path = 'C:/Users/Admin/Downloads/test'
-        os.startfile(folder_path)
+    if base_folder.strip() == "":
+        messagebox.showwarning("Warning", "File path is required")
     else:
-        messagebox.showerror("Error", "Invalid file path")
+        if re.match(base_folder, regex):
+            merged_data = []
+            # Walk through all folders and subfolders
+            for root, dirs, files in os.walk(base_folder):
+                for file in files:
+                    if file.endswith('.json'):
+                        file_path = os.path.join(root, file)
+                        # Open and read each JSON file
+                        with open(file_path, 'r', encoding='utf-8') as f:
+                            data = json.load(f)
+                            # If the data is a list, extend the merged list, otherwise append
+                            if isinstance(data, list):
+                                merged_data.extend(data)
+                            else:
+                                merged_data.append(data)
+
+            def clean_content(content):
+                if len(content) == 0:
+                    return None
+                content_df = pd.DataFrame(content)[["channel", "text"]]
+                return content_df.to_dict("records")
+
+            def clean_meta(metadata):
+                metadata = eval(
+                    metadata.replace("true", "True")
+                    .replace("false", "False")
+                    .replace("null", "None")
+                )["public"]
+                return metadata
+
+            # Write the merged data to a new JSON file
+            path = Path(base_folder)
+            file_name = path.name
+            output_file = f'C:/Users/Admin/Downloads/test/{file_name}.json'
+            with open(output_file, 'w', encoding='utf-8') as f:
+                json.dump(merged_data, f, indent=4, ensure_ascii=False)
+            # print(f"Data from JSON files in multiple folders has been merged into {output_file}")
+
+            if __name__ == "__main__":
+                file_name = output_file
+                data_call = pd.read_json(file_name)[["name", "agentChannel", "content", "metadata"]]
+
+                data_call["content"] = data_call["content"].apply(clean_content)
+                data_call["metadata"] = data_call["metadata"].apply(clean_meta)
+                data_call.to_json(
+                    file_name.split(".")[0] + "_clean.json",
+                    orient="records",
+                    force_ascii=False,
+                    indent=2,
+                )
+            messagebox.showinfo("Successfully", f"Name Transcript: {file_name}")
+            folder_path = 'C:/Users/Admin/Downloads/test'
+            os.startfile(folder_path)
+        else:
+            messagebox.showerror("Error", "Invalid file path")
 
 def exit_app():
     # Kiểm tra nếu tất cả các trường đều trống
@@ -76,7 +78,7 @@ def exit_app():
     #     messagebox.showwarning("Warning", "Vui lòng hoàn tất thông tin hoặc xóa hết để thoát.")
 def clear_text():
     if not entry_base_folder.get():
-        messagebox.showwarning("Warning","File path is required")
+        messagebox.showwarning("Warning", "File path is required")
     else:
         entry_base_folder.delete(0, tk.END)
 
@@ -97,7 +99,7 @@ background_photo = ImageTk.PhotoImage(background_image)
 background_label = tk.Label(root, image=background_photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Tạo text bõ nhập file path
+# Tạo text box nhập file path
 label_name = tk.Label(root, text="File path:")
 label_name.grid(row=0, column=0, padx=10, pady=10)
 
