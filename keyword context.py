162e3f53b6619aafc26918e863183f3f754b2d8b
hungf12 <93297529+hungf12@users.noreply.github.com>
2024-12-23 16:35:46 +0700

    add new script
    23/12/2024 => done

---
+++ keyword context.py
@@ -0,0 +1,213 @@
+data = [{
+    "fileName": "QVJR6F8NI94010LRFDI6FIFHMG1V3NJE.mp3",
+    "agentChannel": 1,
+    "time_load_info": 0.003962039947509766,
+    "criterias_order": [],
+    "context_analysis": {
+      "searchInvalidKeyword": [
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 3,
+          "channel": 1,
+          "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 4,
+          "channel": 1,
+          "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "keywords": [],
+          "index": 5,
+          "channel": 1,
+          "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        }
+      ]
+    },
+    "searchInvalidKeyword": {
+      "evaluate": "yes",
+      "evaluate_channel": {
+        "1": "yes",
+        "2": "no"
+      },
+      "decision_position": [
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 3,
+          "channel": 1,
+          "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 4,
+          "channel": 1,
+          "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "keywords": [],
+          "index": 5,
+          "channel": 1,
+          "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        }
+      ],
+      "decision": "yes",
+      "confidence": 1.0,
+      "confidence_channel": {
+        "1": 1.0,
+        "2": 0.0
+      },
+      "position": [
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 3,
+          "channel": 1,
+          "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "alo",
+          "keywords": [],
+          "index": 4,
+          "channel": 1,
+          "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        },
+        {
+          "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "keywords": [],
+          "index": 5,
+          "channel": 1,
+          "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+          "intent": "impolite",
+          "intents": [
+            "impolite"
+          ]
+        }
+      ],
+      "slots": {},
+      "position_range_index": [],
+      "position_condition": [],
+      "task": "context_analysis"
+    },
+    "BUCKET_OR_REGION": "B3"
+  }]
+
+kw = [
+  {
+    "bucket": "Predue",
+    "items": [
+      "ăn cơm hay ăn",
+      "con kia",
+      "âm mưu lừa đảo",
+      "âm mưu trốn nợ",
+      "ăn của người ta",
+      "ăn dốc nói láo",
+      "ăn gì tôi cúng",
+      "ăn gì tui cúng",
+      "ăn giựt tiền",
+      "ăn quỵt tiền"
+    ]
+  },
+  {
+    "bucket": "B0 Promo",
+    "items": [
+      "ăn cơm hay ăn",
+      "con kia",
+      "âm mưu lừa đảo",
+      "âm mưu trốn nợ",
+      "ăn của người ta",
+      "ăn dốc nói láo",
+      "ăn gì tôi cúng",
+      "ăn gì tui cúng",
+      "ăn giựt tiền",
+      "ăn quỵt tiền"
+    ]
+  },
+  {
+    "bucket": "B3",
+    "items": [
+      "ăn cơm hay ăn",
+      "con kia",
+      "âm mưu lừa đảo",
+      "âm mưu trốn nợ",
+      "ăn của người ta",
+      "ăn dốc nói láo",
+      "ăn gì tôi cúng",
+      "ăn gì tui cúng",
+      "ăn giựt tiền",
+      "im lặng"
+    ]
+  }
+]
+
+result = []
+for item in data:
+    # nếu decision trả về "no" thì result = yes do không tìm thấy keyword hoặc intent = polite
+    if (item["searchInvalidKeyword"]["decision"]) == "no":
+        result.append({
+            "type": "case không vi phạm",
+            "file_name": item["fileName"],
+            "result": "Yes"
+        })
+    else:
+      for entry in data:
+        bucket_or_region = entry.get("BUCKET_OR_REGION")
+        text = " ".join([item["text"] for item in entry["context_analysis"]["searchInvalidKeyword"]])
+
+        match_found = False
+        invalid_keyword = ""
+        for bucket in kw:
+          if bucket["bucket"] == bucket_or_region:
+            for keyword in sorted(bucket["items"], key=len, reverse=True):
+              if keyword in text:
+                match_found = True
+                invalid_keyword = keyword
+                break
+          if match_found:
+            break
+
+        result.append({
+          "fileName": entry["fileName"],
+          "result": "yes" if match_found else "no",
+          "invalid_keyword": invalid_keyword if match_found else None
+        })
+
+print(result)
--- test kw context.py
+++ test kw context.py
@@ -2,18 +2,40 @@
 import json
 
 data = [{
-    "fileName": "B5D5P6L9J574P8VDBK9LQG9Q9C2G903B.mp3",
+    "fileName": "QVJR6F8NI94010LRFDI6FIFHMG1V3NJE.mp3",
     "agentChannel": 1,
-    "time_load_info": 0.004342079162597656,
+    "time_load_info": 0.003962039947509766,
     "criterias_order": [],
     "context_analysis": {
         "searchInvalidKeyword": [
             {
-                "raw_text": "mình đi vay tiền mà sao không có khả năng đóng là sao anh bảo hiểm nào chị vậy",
+                "raw_text": "alo",
                 "keywords": [],
-                "index": 10,
+                "index": 3,
                 "channel": 1,
-                "text": "ủa cái khoản vay của anh ở bên fe sao chưa ra thanh toán vậy lý do vì sao vậy anh mình đi vay tiền mà sao không có khả năng đóng là sao anh bảo hiểm nào chị vậy hả nãy giờ anh mới là người nói chuyện mất dạy á chứ không phải em đâu",
+                "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "alo",
+                "keywords": [],
+                "index": 4,
+                "channel": 1,
+                "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "keywords": [],
+                "index": 5,
+                "channel": 1,
+                "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
                 "intent": "impolite",
                 "intents": [
                     "impolite"
@@ -29,11 +51,33 @@ data = [{
         },
         "decision_position": [
             {
-                "raw_text": "mình đi vay tiền mà sao không có khả năng đóng là sao anh bảo hiểm nào chị vậy",
+                "raw_text": "alo",
                 "keywords": [],
-                "index": 10,
+                "index": 3,
                 "channel": 1,
-                "text": "ủa cái khoản vay của anh ở bên fe sao chưa ra thanh toán vậy lý do vì sao vậy anh mình đi vay tiền mà sao không có khả năng đóng là sao anh bảo hiểm nào chị vậy hả nãy giờ anh mới là người nói chuyện mất dạy á chứ không phải em đâu",
+                "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "alo",
+                "keywords": [],
+                "index": 4,
+                "channel": 1,
+                "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "keywords": [],
+                "index": 5,
+                "channel": 1,
+                "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
                 "intent": "impolite",
                 "intents": [
                     "impolite"
@@ -45,36 +89,91 @@ data = [{
         "confidence_channel": {
             "1": 1.0,
             "2": 0.0
-        }
+        },
+        "position": [
+            {
+                "raw_text": "alo",
+                "keywords": [],
+                "index": 3,
+                "channel": 1,
+                "text": "alo phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "alo",
+                "keywords": [],
+                "index": 4,
+                "channel": 1,
+                "text": "phạm chí nam đang nghe máy đúng không ạ bên fe alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            },
+            {
+                "raw_text": "bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "keywords": [],
+                "index": 5,
+                "channel": 1,
+                "text": "alo alo bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau",
+                "intent": "impolite",
+                "intents": [
+                    "impolite"
+                ]
+            }
+        ],
+        "slots": {},
+        "position_range_index": [],
+        "position_condition": [],
+        "task": "context_analysis"
     }
 }]
 
 # Mảng transcript
 transcript = [{
-    "name": "B5D5P6L9J574P8VDBK9LQG9Q9C2G903B.mp3",
-    "agentChannel": 1,
-    "content": [
-        {
-            "channel": 2,
-            "text": "alo"
-        },
-        {
-            "channel": 1,
-            "text": "ờ alo cho hỏi phải số của mai thanh không anh"
-        }
+    "name":"QVJR6F8NI94010LRFDI6FIFHMG1V3NJE.mp3",
+    "agentChannel":1,
+    "content":[
+      {
+        "channel":1,
+        "text":"alo cho mình hỏi phải phạm chí nam đang nghe máy không"
+      },
+      {
+        "channel":1,
+        "text":"alo"
+      },
+      {
+        "channel":1,
+        "text":"phạm chí nam đang nghe máy đúng không ạ bên fe"
+      },
+      {
+        "channel":1,
+        "text":"alo"
+      },
+      {
+        "channel":1,
+        "text":"alo"
+      },
+      {
+        "channel":1,
+        "text":"bắt máy để im lặng gác máy trước bên phía ngân hàng sẽ tiếp tục gọi lại sau"
+      }
     ],
-    "metadata": {
-        "duration": "130532",
-        "accessgroups": "[\"/COLL/FIELD/LATE-CALL-SUP/TEAM_LOAN_B2_LATE CALL\"]",
-        "BUCKET_OR_REGION": "B4B6",
-        "GSW_CAMPAIGN_NAME": "COL_EC1_AG04_PEGA_CAMPAIGN",
-        "USER_GENESYS": "voyvi",
-        "startTime": "2024-11-28T09:10:58.000+0000",
-        "LINE_MANAGER_1_TEAMLEADER": "Bùi Quốc Thành",
-        "id": "B5D5P6L9J574P8VDBK9LQG9Q9C2G903B",
-        "username": "voyvi"
+    "metadata":{
+      "duration":"32390",
+      "BUCKET_OR_REGION":"B3",
+      "agentId":"29190",
+      "GSW_CAMPAIGN_NAME":"COL_EC1_AG13_PEGA_CAMPAIGN",
+      "USER_GENESYS":"doquangdat2",
+      "startTime":"2024-11-27T07:53:06.000+0000",
+      "LINE_MANAGER_1_TEAMLEADER":"Nguyễn Thị Bích Hạnh",
+      "id":"QVJR6F8NI94010LRFDI6FIFHMG1V3NJE",
+      "username":"doquangdat2"
     }
-}]
+  },]
 # Kiểm tra và gán BUCKET_OR_REGION từ transcript vào data nếu trùng fileName và name
 for item_data in data:
     for item_transcript in transcript:
